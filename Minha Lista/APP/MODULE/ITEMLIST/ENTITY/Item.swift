d3aeedc1e0d7c2e7e5fff72a42fa3ddb7e535446
Wilton Garcia <wiltongarciaweb@gmail.com>
2021-05-18 17:17:35 -0300

    Entidades criadas

---
+++ Minha Lista/APP/MODULE/ITEMLIST/ENTITY/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Minha Lista
+//
+//  Created by Wilton Garcia on 18/05/21.
+//
+
+import Foundation
+
+class Item{
+    private let ItemName: String
+    
+    init(ItemName: String) {
+        self.ItemName = ItemName
+    }
+}
---
+++ Minha Lista/APP/MODULE/ITEMLIST/ENTITY/ItemList.swift
@@ -0,0 +1,18 @@
+//
+//  ItemList.swift
+//  Minha Lista
+//
+//  Created by Wilton Garcia on 18/05/21.
+//
+
+import Foundation
+
+class ItemList {
+    private let ListName: String
+    private let ItemList = [Item]()
+    
+    
+    init(ListName: String) {
+        self.ListName = ListName
+    }
+}
