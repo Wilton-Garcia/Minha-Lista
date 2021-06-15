6f921565e5ce370b098e60dded5644cd4380cc26
Wilton Garcia <wiltongarciaweb@gmail.com>
2021-06-14 23:02:34 -0300

    Popup view para a criação de itens dentro da lista

---
+++ Minha Lista/APP/MODULES/ADDITEM/VIEW/AddItemView.swift
@@ -0,0 +1,142 @@
+//
+//  AddItemView.swift
+//  Minha Lista
+//
+//  Created by Wilton Garcia on 14/06/21.
+//
+
+import UIKit
+
+class AddItemView: UIView {
+    //MARK: - Private properties
+    
+    public weak var delegate: AddListViewDelegate?
+    
+    private var clique = 0;
+    
+    //MARK: - View Properties
+    
+    private let contentView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let labelItemName: UILabel = {
+        let label = UILabel()
+        label.text = "Digite um nome do item"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let textFieldItemtName: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Lista de ..."
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    private let buttonCreateItem: UIButton = {
+        let button = UIButton()
+        button.setTitle("Cadastrar", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(addList), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let buttonDimiss: UIButton = {
+        let button = UIButton()
+        button.setTitle("Cancelar", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(close), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    //MARK: - View Methods
+
+    private func setupContentView(){
+        self.addSubview(contentView)
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: topAnchor, constant: 300),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -300),
+        ])
+    }
+    
+    private func setupLabelListName(){
+        contentView.addSubview(labelItemName)
+        NSLayoutConstraint.activate([
+            labelItemName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            labelItemName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            labelItemName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
+        ])
+    }
+    
+    private func setupTextFieldListName(){
+        contentView.addSubview(textFieldItemtName)
+        NSLayoutConstraint.activate([
+            textFieldItemtName.heightAnchor.constraint(equalToConstant: 30),
+            textFieldItemtName.topAnchor.constraint(equalTo: labelItemName.bottomAnchor, constant: 15),
+            textFieldItemtName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            textFieldItemtName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
+        ])
+    }
+    
+    private func setupButtonCreateList(){
+        contentView.addSubview(buttonCreateItem)
+        NSLayoutConstraint.activate([
+            buttonCreateItem.heightAnchor.constraint(equalToConstant: 20),
+            buttonCreateItem.topAnchor.constraint(equalTo: textFieldItemtName.bottomAnchor, constant: 15),
+            buttonCreateItem.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            buttonCreateItem.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
+        ])
+    }
+    
+    private func setupButtonDimiss(){
+        contentView.addSubview(buttonDimiss)
+        NSLayoutConstraint.activate([
+            buttonDimiss.heightAnchor.constraint(equalToConstant: 20),
+            buttonDimiss.topAnchor.constraint(equalTo: buttonCreateItem.bottomAnchor, constant: 20),
+            buttonDimiss.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30),
+            buttonDimiss.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30)
+        ])
+    }
+    
+    @objc private func close(){
+        delegate?.closePopup()
+    }
+    
+    @objc private func addList(){
+        if let listName = textFieldItemtName.text{
+            delegate?.addList(listName: listName)
+        }
+    }
+    
+    private func setupLayout(){
+        setupContentView()
+        setupLabelListName()
+        setupTextFieldListName()
+        setupButtonCreateList()
+        setupButtonDimiss()
+        self.backgroundColor = .clear
+    }
+    
+    //MARK: - Init
+    
+    convenience init() {
+        self.init(frame:.zero)
+        setupLayout()
+    }
+    
+    //MARK: - View Methods
+    
+    @objc public func closeView(){
+        delegate?.closePopup()
+        
+    }
+}
---
+++ Minha Lista/APP/MODULES/ADDITEM/VIEW/AddItemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddItemViewController.swift
+//  Minha Lista
+//
+//  Created by Wilton Garcia on 14/06/21.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
